a4b15a0d94c6c19ee252ae70a807c3f4f4670697
mtyra321 <mtyra321@gmail.com>
2021-03-11 17:33:18 -0700

    did project

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const function_controller = require('./functions.js');
 
+
+
 express()
     .use(express.static(path.join(__dirname, 'public')))
     .use(express.urlencoded({ extended: true })) //support url encoded bodies
@@ -11,3 +13,14 @@ express()
     .get('/', (req, res) => res.render('pages/index'))
     .post('/results', function_controller.math)
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+// (require("http").createServer(function(req, res) {
+//     var md5 = require("blueimp-md5"),
+//         url = require("url"),
+//         query = url.parse(req.url).query;
+//     res.writeHead(200, { " Content - Type": "text / plain" })
+
+//     // compute and print the MD5 hash of the url query
+//     res.end(md5(query))
+// }).listen(8080, "localhost"))
--- public/Project/search.js
+++ public/Project/search.js
@@ -1,43 +1,127 @@
-const url = "https://pokeapi.co/api/v2/type/3";
-const key = "f4115560196924074bec30f1c2fb50c8";
+//  import md5 from 'js-md5';
+//  import md5;
 
-function convertToJson(response) {
-    if (response.ok == true) {
-        return response.json();
-    } else {
-        throw new Error(response.statusText);
+const url = "https://gateway.marvel.com:443/v1/public/";
+
+const key = "f4115560196924074bec30f1c2fb50c8";
+const pkey = '534e8d4706e61fe271dead8840c7ee181fd87fe5';
+
+function onload() {
+    document.getElementById("button").addEventListener("click", search)
+    document.getElementById("searchBar").addEventListener("keydown", function(e) {
+        if (e.key === "Enter") { //checks whether the pressed key is "Enter"
+            e.preventDefault();
+            search(e);
+        }
+
+    });
+    var web = '';
+}
+
+function search(event) {
+    console.log("search");
+    console.log("event is = " + event);
+    var timestamp = event.timeStamp;
+    var name = document.getElementById("searchBar").value;
+    var hash = md5(timestamp + pkey + key);
+
+    //            var hash = md5(timestamp + pkey + key);
+    var type = document.getElementById("type").value;
+    var urlType = "";
+    console.log("type is " + type);
+    if (type == "character") {
+        urlType = 'characters?nameStartsWith=';
+    } else if (type == "comic") {
+        urlType = 'comics?titleStartsWith=';
+
+    } else if (type == "series") {
+        urlType = 'series?titleStartsWith=';
+
+    } else if (type == "event") {
+        urlType = "events?nameStartsWith=";
+    }
+    web = url + urlType + name + '&ts=' + timestamp + '&apikey=' + key + '&hash=' + hash;
+
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+            var results = response.data.results;
+            console.log(results);
+            removeAllChildNodes(document.getElementById("results"));
+            for (let index = 0; index < results.length; index++) {
+                const element = results[index];
+                document.getElementById("results").appendChild(display_single_result(type, element));
+            }
+        }
+    };
+    xmlhttp.open("GET", web, true);
+    xmlhttp.send();
+    event.preventDefault();
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
     }
 }
 
-async function getPokemonAsync(url) {
-    let pokemonData = await fetch(url).then(convertToJson);
-    let pokemon = pokemonData.pokemon;
-    displayPokemon(pokemon);
+function display_single_result(type, result) {
+    if (type == "character") {
+        return display_single_character(result);
+    } else if (type == "comic") {
+        return display_single_comic(result);
+
+    } else if (type == "series") {
+        return display_single_comic_series(result);
+
+    } else if (type == "event") {
+        return display_single_event(result);
+    }
 }
 
-function displayPokemon(list) {
-    // console.log(list);
-    const listElement = document.getElementById("theList");
 
-    const newArray = list.map((item) => {
-        return `<li data-url = "${item.pokemon.url}">${item.pokemon.name}</li>`;
-    });
-    listElement.innerHTML = newArray.join("");
-    //   console.log(newArray);
-
+function display_single_character(result) {
+    console.log("displaying single result");
+    console.log(result);
+    let div = document.createElement('div');
+    div.classList.add("result");
+    let name = document.createElement('p');
+    name.innerText = result.name;
+    div.appendChild(name);
+    return div;
 }
 
-async function pokemonClicked(event) {
-    console.log(event.target.dataset.url);
-    const details = await fetch(event.target.dataset.url).then(convertToJson);
-    console.log(details);
-    //   console.log(currentTarget);
-    displayDetails(details);
+function display_single_comic(result) {
+    console.log("displaying single result");
+    console.log(result);
+    let div = document.createElement('div');
+    div.classList.add("result");
+    let name = document.createElement('p');
+    name.innerText = result.title;
+    div.appendChild(name);
+    return div;
 }
 
-function displayDetails(details) {
-    let section = document.getElementById("details");
+function display_single_comic_series(result) {
+    console.log("displaying single result");
+    console.log(result);
+    let div = document.createElement('div');
+    div.classList.add("result");
+    let name = document.createElement('p');
+    name.innerText = result.title;
+    div.appendChild(name);
+    return div;
 }
 
-getPokemonAsync(url);
-document.getElementById("theList").addEventListener('click', pokemonClicked);
+function display_single_event(result) {
+    console.log("displaying single result");
+    console.log(result);
+    let div = document.createElement('div');
+    div.classList.add("result");
+    let name = document.createElement('p');
+    name.innerText = result.title;
+    div.appendChild(name);
+    return div;
+}
